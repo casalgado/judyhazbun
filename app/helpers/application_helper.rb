5d5a52cfe8228cff7ac04cdd8114aa116caad235
Carlos Salgado <casalgado86@yahoo.com>
2015-02-23 12:55:19 -0500

    modal images not loading in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def display_image(image_name)
-		content_tag(:a, image_tag("#{image_name}"), class: "grid-image", id: "#{Rails.root}/app/assets/images/#{image_name.gsub('grid', 'modal')[0..-5]}")
+		content_tag(:a, image_tag("#{image_name}"), class: "grid-image", id: "#{image_name.gsub('grid', 'modal')[0..-5]}")
 	end
 
 
