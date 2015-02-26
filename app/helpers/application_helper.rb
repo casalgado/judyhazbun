fee690778ded87437d2f757e04a642e229c6da23
Carlos Salgado <casalgado86@yahoo.com>
2015-02-26 11:51:48 -0500

    fixing modal bug in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def display_image(image_name)
-		content_tag(:a, image_tag("#{image_name}"), class: "grid-image", id: "#{image_name.gsub('grid', 'modal')[0..-5]}")
+		content_tag(:a, image_tag("#{image_name}"), class: "grid-image", id: "#{image_name.split("/")[-1][0..-5]}")
 	end
 
 	def display_modal_image(image_name)
